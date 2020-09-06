4c6698adffd069ea1c687f03b1326b531b85b88e
sriram-varma <70637117+sriram-varma@users.noreply.github.com>
2020-09-06 19:57:16 +0530

    Add files via upload

---
+++ arithmetic.cs
@@ -0,0 +1,12 @@
+int a = 18;
+int b = 6;
+int c = a + b;
+int d = a - b;
+int e = a * b;
+int f = a / b;
+int g = a % b;
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
